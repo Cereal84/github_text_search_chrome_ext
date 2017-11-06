c2632fff772025653d3b3cf7517b688bf91e11dc
Alessandro Pischedda <a.pischedda@nextworks.it>
2017-11-06 16:26:08 +0100

    add replace

--- content.js
+++ content.js
@@ -40,6 +40,14 @@ function store_last_search(text_search, matches)
 }
 
 
+function highlight(container,what,spanClass) {
+    var content = container.innerHTML,
+        pattern = new RegExp('(>[^<.]*)(' + what + ')([^<.]*)','g'),
+        replaceWith = '$1<span ' + ( spanClass ? 'class="' + spanClass + '"' : '' ) + '">$2</span>$3',
+        highlighted = content.replace(pattern,replaceWith);
+    return (container.innerHTML = highlighted) !== content;
+}
+
 
 function search_and_highlight_text(container, search_for)
 {
