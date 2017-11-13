d4363f13ea10469b1a7915f2b86633ebe75802cb
Alessandro Pischedda <a.pischedda@nextworks.it>
2017-11-13 14:50:26 +0100

    popup get 'default color' if not storage exists

--- popup.js
+++ popup.js
@@ -1,3 +1,33 @@
 
-colorPicker.addEventListener("change", watchColorPicker, false);
+window.addEventListener ("load", main, false);
+
+
+function color_changed(event)
+{
+    var background_color = event.target.value;
+
+}
+
+function main(evt) {
+
+    var match_color = "#fff0b3";
+
+    var storage = window.localStorage.getItem("gh_text_search");
+    // check if background color is stored
+    if (storage !== null)
+    {
+        var data = storage['settings'];
+        // get stored color from settings
+        match_color = data["settings"]['background_match'];
+    }
+
+
+    color_select = document.getElementById("gh_ext_color");
+
+    color_select.value = match_color;
+    color_select.addEventListener("change", color_changed, false);
+
+
+}
+
 
