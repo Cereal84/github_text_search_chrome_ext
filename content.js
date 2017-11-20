3a16db6e0968b8a1eb08f010c69ae40be7a5740e
Alessandro Pischedda <a.pischedda@nextworks.it>
2017-11-20 16:39:06 +0100

    various mess

--- content.js
+++ content.js
@@ -18,6 +18,8 @@ window.addEventListener ("load", main, false);
 // global variable used to loop on focus elements
 var current_focus_index = 0;
 var total_matches = 0;
+var match_color = null;
+
 
 function first_focus()
 {
@@ -142,23 +144,46 @@ function store_last_search(text_search, matches)
 }
 
 
-function highlight(container, what, match_color) {
+function replace_match(match, p1)
+{
 
-    var content = container.innerText;
-    if (content == "")
+
+    // TODO check if the match is inside a tag, in that case ignore it
+
+    var id_name = "gh_ext_match_" + total_matches;
+
+    new_value = '<span id="' + id_name + '"' +
+                   'tabindex="0" style="background:' + match_color +'">'+p1+'</span>';
+
+    // update total matches
+    total_matches++;
+
+    return new_value;
+}
+
+
+function highlight(container, what) {
+
+    if (container == undefined)
+        return;
+
+
+    // TODO ISSUE: using innerHTML it do replace even inside HTML tags
+    // corrupting the tag
+
+    var content = container.textContent;
+
+    if ((content == "") || (content == undefined))
         return;
 
     var id_name = "gh_ext_match_" + total_matches;
     var  pattern = new RegExp('(' + what + ')','ig');
 
+    var new_content = content.replace(pattern, replace_match);
 
-    // tabindex is still 0 because the focus is handeld by hand
-    var new_content = content.replace(pattern, '<span id="' + id_name + '"' +
-                   'tabindex="0" style="background:' + match_color +'">$1</span>');
     if (new_content != content )
     {
         container.innerHTML = new_content;
-        total_matches++;
     }
 
 }
@@ -213,7 +238,6 @@ function search_in_table_file(container, search_for)
     // class="highlight tab-size js-file-line-container"
     // container is div with itemprop=text
 
-    var match_color = null;
 
     chrome.runtime.sendMessage({type: "getMatchColor"}, function(response) {
 
@@ -227,7 +251,7 @@ function search_in_table_file(container, search_for)
 
                 for (var j = 0, col; col = row.cells[j]; j++) {
 
-                    highlight(col, search_for, match_color);
+                    highlight(col, search_for);
 
                 }// end cells
             } // end ROWS
@@ -239,22 +263,63 @@ function search_in_table_file(container, search_for)
 }
 
 
+
+
 function search_and_highlight_text(container, search_for)
 {
 
+    // ignore every node whis isn't an element node
+    if (container.nodeType != Node.ELEMENT_NODE)
+    {
+        console.log("I'm not a node element");
+        return;
+    }
     var children = container.childNodes;
 
+    console.log(container);
+    console.log(" has child n ");
+    console.log(children.length);
     if (children.length == 0)
     {
         // search the text
-        var content = container;
-
+        console.log("HIGHLIGHT");
+        highlight(container, search_for);
+        return;
     } else {
-        for (i = 0; i < children.length; i++)
+        for (i = 0; i < 2; i++)
+        {
+            if (children[i] == undefined)
+                continue;
             search_and_highlight_text(children[i], search_for);
+        }
     }
 
 }
+
+
+function search_in_article_file(container, search_for)
+{
+    // these files have a table
+    // class="highlight tab-size js-file-line-container"
+    // container is div with itemprop=text
+
+    chrome.runtime.sendMessage({type: "getMatchColor"}, function(response) {
+
+        match_color = response.data;
+
+        article = document.querySelector('[class="markdown-body entry-content"]');
+        if (article)
+        {
+            highlight(article, search_for);
+        }
+
+        first_focus();
+        create_match_div();
+    });
+
+
+}
+
 
 
 function highlight_matches_on_current_file()
@@ -287,8 +352,19 @@ function highlight_matches_on_current_file()
     // check which kinfd of file we're talking about (code, markup ecc)
     //itemprop="text"
     var container =  document.querySelectorAll('[itemprop="text"]');
-    if (container)
-        search_in_table_file(container[0], search_for);
+    if (container) {
+
+        if(document.querySelector('[class="markdown-body entry-content"]'))
+        {
+            console.log("search in ARTICLE");
+            search_in_article_file(container[0], search_for);
+        }
+        else
+        {
+           search_in_table_file(container[0], search_for);
+        }
+
+    }
         //search_and_highlight_text(container[0], search_for);
 
 }
