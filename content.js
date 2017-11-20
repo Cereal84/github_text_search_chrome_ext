650555ec1ff556463666d227314821738482a13c
Alessandro Pischedda <alessandro.pischedda@gmail.com>
2017-11-20 20:01:39 +0100

    moved code from content.js to view.js

--- content.js
+++ content.js
@@ -32,14 +32,6 @@ function first_focus()
 }
 
 
-function update_focus_infos()
-{
-
-    var span_infos = document.getElementById("focus_infos");
-    var text = "[" + (current_focus_index + 1 ) + "/" + total_matches + "]";
-    span_infos.innerHTML = text;
-}
-
 function go_to_prev_focus()
 {
 
@@ -122,7 +114,7 @@ document.addEventListener('keyup', focus_keyUp, false);
 function clean_page()
 {
     // clean table_elem
-    var table_elem = document.getElementById("ext_matches");
+    var table_elem = document.getElementById( ID_EXT_MATCHES );
     while (table_elem.hasChildNodes()){
         table_elem.removeChild(table_elem.firstChild);
     }
@@ -205,49 +197,6 @@ function highlight(container, what) {
 
 }
 
-
-function create_match_div()
-{
-    // its position is to bottom right of the window
-    var panel = document.createElement("div");
-    panel.setAttribute('id', 'gh_match_next_prev');
-    //panel.setAttribute('class', 'panel panel-default');
-    panel.style.bottom = "40px";
-    panel.style.right = "50px";
-    panel.style.zIndex = "999";
-    panel.style.position = "fixed";
-    panel.style.border = "2px solid #c8e1ff";
-
-    var panel_body = document.createElement("div");
-    panel_body.style.margin = "3px";
-    // button PREV
-    var button_prev = document.createElement("button");
-    var prev_text = document.createTextNode("Prev");
-    button_prev.appendChild(prev_text);
-    button_prev.onclick = go_to_prev_focus;
-
-    // button NEXT
-    var button_next = document.createElement("button");
-    var next_text = document.createTextNode("Next");
-    button_next.appendChild(next_text);
-    button_next.onclick = go_to_next_focus;
-
-    // info about focused match / total_matches
-    var focus_match_infos = document.createElement("span");
-    focus_match_infos.setAttribute("id", "focus_infos");
-    focus_match_infos.innerHTML = "[1/" + total_matches + "]";
-    focus_match_infos.style.marginLeft = "2px";
-
-    panel_body.appendChild(button_prev);
-    panel_body.appendChild(button_next);
-    panel_body.appendChild(focus_match_infos);
-
-    panel.appendChild(panel_body);
-
-
-    document.body.appendChild(panel);
-}
-
 function search_in_table_file(container, search_for)
 {
 
@@ -293,9 +242,6 @@ function search_and_highlight_text(container, search_for)
     }
     var children = container.childNodes;
 
-    console.log(container);
-    console.log(" has child n ");
-    console.log(children.length);
     if (children.length == 0)
     {
         // search the text
@@ -476,33 +422,6 @@ function do_search()
 
 }
 
-function create_match_container()
-{
-    var matches_div = document.createElement("div");
-    matches_div.setAttribute('class', 'ext_matchs_div');
-    matches_div.className += " boxed-group clearfix announce";
-
-    var title_h3 = document.createElement("h3");
-    var text = document.createTextNode("Matches");
-    title_h3.appendChild(text);
-
-    var file_wrap_div = document.createElement('div');
-    file_wrap_div.setAttribute('class', 'file-wrap');
-
-    // create table
-    var table_elem = document.createElement("table");
-    table_elem.setAttribute('id', 'ext_matches');
-    table_elem.className += "files js-navigation-container js-active-navigation-container";
-
-    file_wrap_div.appendChild(table_elem);
-    matches_div.appendChild(title_h3);
-
-    matches_div.appendChild(file_wrap_div);
-    return matches_div;
-
-}
-
-
 function keypress_handler(event)
 {
     // enter has keyCode = 13, change it if you want to use another button
@@ -533,63 +452,6 @@ function get_last_search()
 
     return result;
 
-}
-
-function create_ext_search_div(file_nav_node)
-{
-
-        var search_div = document.createElement("div");
-        search_div.className += "file";
-
-        var container_div = document.createElement("div");
-        container_div.setAttribute('id', 'ext_search_container')
-        container_div.setAttribute('class', 'file-header');
-
-        var search_label = document.createElement("label");
-        var label_text = document.createTextNode("Search for: ");
-
-        var input = document.createElement("input");
-        input.setAttribute('type', 'text');
-        input.setAttribute('id', 'ext_search_bar');
-        input.setAttribute('class', 'form-control');
-        input.setAttribute('placeholder', 'Search into code');
-
-        input.addEventListener("keypress", keypress_handler);
-
-        // span with searching info list "start"/"end"
-        var span_info = document.createElement("span");
-        span_info.setAttribute('id', 'gh_match_infos');
-        span_info.style.marginLeft = "5px";
-
-        container_div.appendChild(label_text);
-        container_div.appendChild(input);
-        container_div.appendChild(span_info);
-
-        search_div.appendChild(container_div);
-        var file_wrap_div = document.createElement('div');
-
-        // create table
-        var table_elem = document.createElement("table");
-        table_elem.setAttribute('id', 'ext_matches');
-        table_elem.className += "files js-navigation-container js-active-navigation-container";
-
-        file_wrap_div.appendChild(table_elem);
-
-        search_div.appendChild(file_wrap_div);
-
-        file_nav_node.parentNode.insertBefore(search_div, file_nav_node);
-        document.getElementById("ext_search_container").style.marginLeft = "auto";
-        document.getElementById("ext_search_container").style.marginRight = "auto";
-
-        var last_search = get_last_search();
-
-        if ((last_search == "") || (last_search == undefined))
-            return;
-
-        // write value on input
-        document.getElementById("ext_search_bar").value = last_search;
-        // redo search
-        do_search();
 }
 
 
